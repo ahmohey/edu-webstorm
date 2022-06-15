fcf6c94342472fe4ef4471750dbf6d0e3e850f9a
ahmohey <kyh44004@gmail.com>
2022-06-15 17:57:26 +0900

    계산기 만들어보는 실습을 해봤습니다.

---
+++ js/ex_script.js
@@ -0,0 +1,34 @@
+function alertMessage() {
+    alert("YepSuop")
+}
+
+
+function over(poto) {
+    poto.src = "media/amoe.png";
+
+}
+
+function out(poto) {
+    poto.src = "media/mmoe.png";
+}
+
+
+function meMessage(){
+    document.write("문자1" + disit + "문자2")
+}
+// let disit = 1234;
+// let string1 = "문자1";
+// let string2 = "문자열2";
+// meMessage()
+/*
+document.write("<h3>Welcome!</h3>");
+document.write("2 + 5는 <br>");
+setTimeout(function () {
+    document.write("<mark>7입니다.</mark>");
+}, 2000)
+*/
+// let result = prompt("당신의 이름은 무엇입니까?")
+// document.write(result)
+// alert("done")
+
+
