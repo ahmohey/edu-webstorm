49f58e69b916dcb939399ca003920ee8be99e80f
ahmohey <kyh44004@gmail.com>
2022-06-15 17:53:14 +0900

    계산기 만들어보는 실습을 해봤습니다.

---
+++ js/ex2.js
@@ -0,0 +1,19 @@
+function getInput() {//id = # , class = .
+    let getA = parseFloat(document.querySelector('#inputA').value);
+    let getB = parseFloat(document.querySelector('#inputB').value);
+    let getOp = document.querySelector('#operator').value;
+
+    let result;
+    if (getOp == 'plus') {
+        result = getA + getB;
+    } else if (getOp == 'minus') {
+        result = getA - getB;
+    } else if (getOp == 'multiply') {
+        result = getA * getB;
+    } else if (getOp == 'divide') {
+        result = getA / getB;
+    }
+
+
+    document.querySelector('#result').value = result;
+}
